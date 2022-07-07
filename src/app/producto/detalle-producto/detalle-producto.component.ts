9da19895297cbcd1d5f436f0564ef51dada5a5bb
lauradillonar <laura.dillon.ar@gmail.com>
2022-07-07 09:37:05 -0300

    Viendo detalle de un producto

--- src/app/producto/detalle-producto/detalle-producto.component.ts
+++ src/app/producto/detalle-producto/detalle-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/service/producto.service';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleProductoComponent implements OnInit {
 
-  constructor() { }
+  producto: Producto = new Producto('',0);
+
+  constructor(
+    private productoService: ProductoService,
+    private activateRoute: ActivatedRoute,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    const id =this.activateRoute.snapshot.params['id'];
+    this.productoService.detail(id).subscribe(
+      data => {
+        this.producto = data;
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,
+          positionClass: 'toast-top-center'
+        });
+        this.volver();
+      }
+    );
   }
 
+  volver(){
+    this.router.navigate(['/']);
+  }
 }
